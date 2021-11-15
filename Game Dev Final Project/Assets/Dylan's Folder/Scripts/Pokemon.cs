f017a21f38cd33d763da3ecf2ae20385e1397acf
masterdd15 <masterdd15@gmail.com>
2021-11-15 11:02:52 -0500

    Added Charmander

---
+++ Game Dev Final Project/Assets/Dylan's Folder/Scripts/Pokemon.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pokemon
+{
+    PokemonBase _base;
+    int level;
+
+    public Pokemon(PokemonBase pBase, int pLevel)
+    {
+        _base = pBase;
+        level = pLevel;
+    }
+
+    public int Attack
+    {
+        //Apparently the formula to calculate XXXX in Pokemon
+        get { return Mathf.FloorToInt((_base.Attack * level) / 100f) + 5; }
+    }
+
+    public int Defense
+    {
+        //Apparently the formula to calculate XXXX in Pokemon
+        get { return Mathf.FloorToInt((_base.Defense * level) / 100f) + 5; }
+    }
+
+    public int SpAttack
+    {
+        //Apparently the formula to calculate XXXX in Pokemon
+        get { return Mathf.FloorToInt((_base.SpDefense * level) / 100f) + 5; }
+    }
+
+    public int SpDefense
+    {
+        //Apparently the formula to calculate XXXX in Pokemon
+        get { return Mathf.FloorToInt((_base.SpDefense * level) / 100f) + 5; }
+    }
+
+    public int Speed
+    {
+        //Apparently the formula to calculate XXXX in Pokemon
+        get { return Mathf.FloorToInt((_base.Speed * level) / 100f) + 5; }
+    }
+
+    //The only different equation
+    public int MaxHP
+    {
+        //Apparently the formula to calculate XXXX in Pokemon
+        get { return Mathf.FloorToInt((_base.MaxHP * level) / 100f) + 10; }
+    }
+}

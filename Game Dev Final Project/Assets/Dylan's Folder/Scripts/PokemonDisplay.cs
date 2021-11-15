f5f846458bade7c42d01b7670038a6321be02885
masterdd15 <masterdd15@gmail.com>
2021-11-15 11:26:15 -0500

    Adding PokemonDisplay
    Code that helps Display Pokedex information

---
+++ Game Dev Final Project/Assets/Dylan's Folder/Scripts/PokemonDisplay.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PokemonDisplay : MonoBehaviour
+{
+    //This is the Pokemon we're displaying
+    public Pokemon currentPokemon;
+
+    //Storing UI Gameobjects
+    public SpriteRenderer myfrontImage;
+    public SpriteRenderer mybackImage;
+
+    public TextMeshPro myName;
+    public TextMeshPro myLV;
+    public TextMeshPro myHP;
+    public TextMeshPro mySpeed;
+    public TextMeshPro myAttack;
+    public TextMeshPro myDefense;
+    public TextMeshPro mySpAttack;
+    public TextMeshPro mySpDefense;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //We will set all the values in Update so we can switch out the Pokemon;
+
+    }
+}
